9ed6ace54dc6575d113853bc5ca3ffd919bbb111
Karl Baker <Karl.J.Baker@gmail.com>
2022-08-29 19:17:05 -0700

    Added the Home,Product,Reports routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar"; 
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import ReportsPage from "./pages/ReportsPage";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<NavBar />} />
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ReportsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReportsPage() {
+  return (
+    <div className='reports'>
+        <h1>Reports Page</h1>
+    </div>
+  )
+}
+
+export default ReportsPage
